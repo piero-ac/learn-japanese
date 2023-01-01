cfdcd50a4fc65065bf19de0a23cd5cb659285152
Piero Coronado <pacoronado01@gmail.com>
2023-01-01 15:39:18 -0500

    Add initial element selectors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const gameHTML = document.querySelector(".game-type");
 const gameTitle = document.querySelector("#game-main h1");
+const imgElement = document.querySelector("#char-img");
+const guessForm = document.querySelector("#guessForm");
+const results = document.querySelector("#results h2");
 
 let gameURL = window.location.search;
 let gameType = gameURL.substring(gameURL.indexOf("=") + 1);

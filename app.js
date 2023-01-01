b61ab45217201759b0b116d4c57afbcd30f73905
Piero Coronado <pacoronado01@gmail.com>
2023-01-01 14:53:34 -0500

    Change title of game based on character set chosen

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const gameHTML = document.querySelector(".game-type");
-let gameURL = window.location.search;
+const gameTitle = document.querySelector("#game-main h1");
 
-gameURL = gameURL.substring(gameURL.indexOf("=") + 1);
-gameHTML.innerHTML = gameURL;
+let gameURL = window.location.search;
+let gameType = gameURL.substring(gameURL.indexOf("=") + 1);
+
+gameTitle.innerText = `Name the ${gameType} character`;
